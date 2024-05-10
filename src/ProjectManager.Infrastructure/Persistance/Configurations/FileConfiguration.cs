3490781f57fe3fd910f525918356ada92e397586
SergiuUser <soroceansergiu10@gmail.com>
2024-05-10 13:58:49 +0300

    feat: add configuration for File and FileType

---
+++ src/ProjectManager.Infrastructure/Persistance/Configurations/FileConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using ProjectManager.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace ProjectManager.Infrastructure.Persistance.Configurations
+{
+    public class FileConfiguration : EntityTypeConfiguration<File>
+    {
+        public FileConfiguration() 
+        {
+            HasKey(f => f.Id);
+
+            Property(f => f.FileName).IsRequired().HasMaxLength(70);
+            Property(f => f.FileData).IsRequired();
+            Property(f => f.IsDeleted).IsRequired();
+            Property(f => f.ProjectTaskId).IsOptional();
+            Property(f => f.FileTypeId).IsOptional();
+
+            Property(f => f.Created).IsOptional();
+            Property(f => f.LastModified).IsOptional();
+
+        }
+    }
+}
