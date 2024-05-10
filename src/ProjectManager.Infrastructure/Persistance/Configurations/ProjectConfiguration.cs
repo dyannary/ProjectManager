40d23e2a82285c815c5ccb2daa638c226434866b
Diana <diana.ribac001@gmail.com>
2024-05-10 12:48:41 +0300

    Revert "Revert "Merge branch 'main' into dev-Diana""
    This reverts commit 7e5446284d8c17a48543f2164df249a02634dd43.

---
+++ src/ProjectManager.Infrastructure/Persistance/Configurations/ProjectConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using ProjectManager.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace ProjectManager.Infrastructure.Persistance.Configurations
+{
+    public class ProjectConfiguration : EntityTypeConfiguration<Project>
+    {
+        public ProjectConfiguration() 
+        {
+            HasKey(p => p.Id);
+            Property(p => p.Name).IsRequired().HasMaxLength(50);
+            Property(p => p.Description).IsRequired().HasMaxLength(50);
+            Property(p => p.IsDeleted).IsRequired();
+            Property(p => p.ProjectEndDate).IsRequired();
+            Property(p => p.ProjectStartDate).IsRequired();
+
+            Property(p => p.Created).IsOptional();
+            Property(p => p.LastModified).IsOptional();
+
+            HasMany(p => p.UserProject)
+                .WithRequired(up => up.Project)
+                .HasForeignKey(up => up.ProjectId);
+
+            HasMany(p => p.ProjectTask)
+                .WithRequired(pt => pt.Project)
+                .HasForeignKey(pt => pt.ProjectId);
+        }
+    }
+}
---
+++ src/ProjectManager.Infrastructure/Persistance/Configurations/UserProjectConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using ProjectManager.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace ProjectManager.Infrastructure.Persistance.Configurations
+{
+    public class UserProjectConfiguration : EntityTypeConfiguration<UserProject>
+    {
+        public UserProjectConfiguration() 
+        {
+            HasKey(up => new { up.UserId, up.ProjectId});
+        }
+    }
+}
