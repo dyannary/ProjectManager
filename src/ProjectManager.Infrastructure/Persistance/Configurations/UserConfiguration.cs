fd59118cf2ebe8f450a1f4968d086e59e7affe22
SergiuUser <soroceansergiu10@gmail.com>
2024-05-10 10:11:52 +0300

    feat: add configuration for entities (Project, UserProject, ProjectState, User, UserProjectTask, Roles)

--- src/ProjectManager.Infrastructure/Persistance/Configurations/UserConfiguration.cs
+++ src/ProjectManager.Infrastructure/Persistance/Configurations/UserConfiguration.cs
@@ -7,13 +7,24 @@ namespace ProjectManager.Infrastructure.Persistance.Configurations
     {
         public UserConfiguration()
         {
-            this.HasKey(u => u.Id);
-            this.Property(u => u.UserName).IsRequired().HasMaxLength(30);
-            this.Property(u => u.Password).IsRequired().HasMaxLength(50);
-            this.Property(u=>u.FirstName).IsRequired().HasMaxLength(50);
-            this.Property(u => u.LastName).IsRequired().HasMaxLength(50);
-            this.Property(u => u.Email).IsRequired().HasMaxLength(50);
-            this.Property(u => u.RoleId).IsRequired();
+            HasKey(u => u.Id);
+            Property(u => u.UserName).IsRequired().HasMaxLength(30);
+            Property(u => u.Password).IsRequired().HasMaxLength(50);
+            Property(u=>u.FirstName).IsRequired().HasMaxLength(50);
+            Property(u => u.LastName).IsRequired().HasMaxLength(50);
+            Property(u => u.Email).IsRequired().HasMaxLength(50);
+            Property(u => u.RoleId).IsRequired();
+
+            Property(u => u.Created).IsOptional();
+            Property(u => u.LastModified).IsOptional();
+
+            HasMany(u => u.UserProject)
+                .WithRequired(up => up.User)
+                .HasForeignKey(up => up.UserId);
+
+            HasMany(u => u.UserProjectTask)
+                .WithRequired(upt => upt.User)
+                .HasForeignKey(upt => upt.UserId);
         }
     }
 }
