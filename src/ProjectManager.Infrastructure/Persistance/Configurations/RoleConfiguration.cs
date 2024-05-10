7e5446284d8c17a48543f2164df249a02634dd43
Diana <diana.ribac001@gmail.com>
2024-05-10 12:42:16 +0300

    Revert "Merge branch 'main' into dev-Diana"
    This reverts commit 745a196563ea06ab401b423d07ca72fcea321487, reversing
    changes made to 05cc05d79d62413027c854c9ec3f44f886aa1a0b.

--- src/ProjectManager.Infrastructure/Persistance/Configurations/RoleConfiguration.cs
+++ src/ProjectManager.Infrastructure/Persistance/Configurations/RoleConfiguration.cs
@@ -1,19 +1,6 @@
-﻿using ProjectManager.Domain.Entities;
-using System.Data.Entity.ModelConfiguration;
-
-namespace ProjectManager.Infrastructure.Persistance.Configurations
+﻿namespace ProjectManager.Infrastructure.Persistance.Configurations
 {
-    public class RoleConfiguration : EntityTypeConfiguration<Role>
+    public class RoleConfiguration
     {
-        public RoleConfiguration() 
-        {
-            HasKey(r => r.Id);
-            Property(r => r.Name).IsRequired().HasMaxLength(20);
-            Property(r => r.Description).IsRequired().HasMaxLength(100);
-
-            HasMany(r => r.Users)
-                .WithRequired(r => r.Role)
-                .HasForeignKey(r => r.RoleId);
-        }
     }
 }
--- src/ProjectManager.Infrastructure/Persistance/Configurations/UserConfiguration.cs
+++ src/ProjectManager.Infrastructure/Persistance/Configurations/UserConfiguration.cs
@@ -11,25 +11,9 @@ namespace ProjectManager.Infrastructure.Persistance.Configurations
             Property(u => u.UserName).IsRequired().HasMaxLength(30);
             Property(u => u.Password).IsRequired().HasMaxLength(50);
             Property(u=>u.FirstName).IsRequired().HasMaxLength(50);
-
             Property(u => u.Email).IsRequired().HasMaxLength(50);
             Property(u => u.RoleId).IsRequired();
             Property(u => u.LastName).IsRequired().HasMaxLength(50);
-
-            Property(u => u.LastName).IsRequired().HasMaxLength(50);
-            Property(u => u.Email).IsRequired().HasMaxLength(50);
-            Property(u => u.RoleId).IsRequired();
-
-            Property(u => u.Created).IsOptional();
-            Property(u => u.LastModified).IsOptional();
-
-            HasMany(u => u.UserProject)
-                .WithRequired(up => up.User)
-                .HasForeignKey(up => up.UserId);
-
-            HasMany(u => u.UserProjectTask)
-                .WithRequired(upt => upt.User)
-                .HasForeignKey(upt => upt.UserId);
         }
     }
 }
