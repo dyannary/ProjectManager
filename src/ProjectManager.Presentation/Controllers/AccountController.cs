28206af67316a8b76b27c54387f73c9f20429520
SergiuUser <soroceansergiu10@gmail.com>
2024-05-14 16:38:12 +0300

    feat: add login page

---
+++ src/ProjectManager.Presentation/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using ProjectManager.Presentation.Models;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace ProjectManager.Presentation.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly IMediator _mediator;
+
+        public AccountController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(model);
+            }
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            return RedirectToAction("Login", "Account");
+        }
+
+    }
+}
