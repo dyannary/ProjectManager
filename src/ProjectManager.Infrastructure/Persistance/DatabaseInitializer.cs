cb0548111aca19434a5d0d6877a3d8febba9fab1
Sergiu Sorocean <135896442+SergiuUser@users.noreply.github.com>
2024-05-28 16:40:56 +0300

    feat: add more seed data and a migration for extending size of project description

--- src/ProjectManager.Infrastructure/Persistance/DatabaseInitializer.cs
+++ src/ProjectManager.Infrastructure/Persistance/DatabaseInitializer.cs
@@ -1,5 +1,6 @@
 ﻿using ProjectManager.Domain.Entities;
 using ProjectManager.Infrastructure.DataSeeder.Seeds;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity.Migrations;
 using System.Linq;
@@ -24,6 +25,18 @@ namespace ProjectManager.Infrastructure.Persistance
 
             if (!context.UserProjectRole.Any())
                 CreateRolesForUserProject(context);
+
+            if (!context.ProjectTaskStates.Any())
+                CreateProjectTaskStates(context);
+
+            if (!context.ProjectTaskTypes.Any())
+                CreateProjectTaskTypes(context);
+
+            if (!context.ProjectStates.Any())
+                CreateProjectStates(context);
+
+            if (!context.Priorities.Any())
+                CreatePriotities(context);
 
             base.Seed(context);
         }
@@ -62,6 +75,62 @@ namespace ProjectManager.Infrastructure.Persistance
             var users = UsersSeed.SeedUsers(adminRole, userRole);
 
             users.ForEach(user => context.Users.Add(user));
+            context.SaveChanges();
+        }
+
+        private void CreateProjectTaskStates(AppDbContext context)
+        {
+            var projectTaskStates = new List<ProjectTaskState>()
+            {
+                new ProjectTaskState {Name = "Done", Description = "Project is done"},
+                new ProjectTaskState {Name = "Pending", Description = "Project is Pending"},
+                new ProjectTaskState {Name = "Rejected", Description = "Project is Rejected"},
+                new ProjectTaskState {Name = "Confirmed", Description = "Project is Confirmed"},
+                new ProjectTaskState {Name = "In Progress", Description = "Project is in Progress"},
+            };
+
+            projectTaskStates.ForEach(pts => context.ProjectTaskStates.Add(pts));
+            context.SaveChanges();
+        }
+
+        private void CreateProjectTaskTypes(AppDbContext context)
+        {
+            var projectTaskType = new List<ProjectTaskType>()
+            {
+                new ProjectTaskType {Name = "Bug", Description = "The type of this project is ,,Bug,,"},
+                new ProjectTaskType {Name = "Feature", Description = "The type of this project is ,,Feature,,"},
+                new ProjectTaskType {Name = "Modify", Description = "The type of this project is ,,Modify,,"},
+            };
+
+            projectTaskType.ForEach(ptt => context.ProjectTaskTypes.Add(ptt));
+            context.SaveChanges();
+        }
+
+        private void CreateProjectStates(AppDbContext context)
+        {
+            var projectStates = new List<ProjectState>()
+            {
+                new ProjectState {Name = "Development", Description = "The state for this project is in development"},
+                new ProjectState {Name = "Maintenance", Description = "The state for this project is in maintenance"},
+                new ProjectState {Name = "Frozen", Description = "The state for this project is frozen"},
+                new ProjectState {Name = "Done", Description = "The state for this project is done"},
+            };
+
+            projectStates.ForEach(ps => context.ProjectStates.Add(ps));
+            context.SaveChanges();
+        }
+
+        private void CreatePriotities(AppDbContext context)
+        {
+            var priorities = new List<Priority>()
+            {
+                new Priority {Name = "Low", PriorityValue = 1},
+                new Priority {Name = "Medium", PriorityValue = 2},
+                new Priority {Name = "High", PriorityValue = 3},
+                new Priority {Name = "Urgent", PriorityValue = 4},
+            };
+
+            priorities.ForEach(p => context.Priorities.Add(p));
             context.SaveChanges();
         }
     }
