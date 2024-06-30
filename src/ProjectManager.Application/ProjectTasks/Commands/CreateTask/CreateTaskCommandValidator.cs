b46bef08f1e61ef7ff145f7153e9aa9d05da9c6a
Diana <diana.ribac001@gmail.com>
2024-06-30 23:56:49 +0300

    refactor: edit modals front and validations

--- src/ProjectManager.Application/ProjectTasks/Commands/CreateTask/CreateTaskCommandValidator.cs
+++ src/ProjectManager.Application/ProjectTasks/Commands/CreateTask/CreateTaskCommandValidator.cs
@@ -7,20 +7,24 @@ namespace ProjectManager.Application.ProjectTasks.Commands.CreateTask
     {
         public CreateTaskCommandValidator() 
         {
-            RuleFor(model => model.Name).NotEmpty().WithMessage("Name cannot be empty")
-                .NotNull().WithMessage("Name cannot be null")
+            RuleFor(model => model.Name).NotEmpty().WithMessage("This field is required.")
+                .NotNull().WithMessage("This field is required.")
                 .MaximumLength(50).WithMessage("Name can have maximum 50 character")
                 .WithName("Name");
 
-            RuleFor(model => model.TaskStateId).NotNull().WithMessage("Task state cannot be null")
+            RuleFor(model => model.AssignedTo).NotNull().WithMessage("This field is required.")
+                .InclusiveBetween(1, 3).WithMessage("This is not a valid task type")
+                .WithName("AssignedTo");
+
+            RuleFor(model => model.TaskStateId).NotNull().WithMessage("This field is required.")
                 .InclusiveBetween(1, 3).WithMessage("This is not a valid task state")
                 .WithName("Task state");
 
-            RuleFor(model => model.TaskTypeId).NotNull().WithMessage("Task type cannot be null")
+            RuleFor(model => model.TaskTypeId).NotNull().WithMessage("This field is required.")
                 .InclusiveBetween(1, 3).WithMessage("This is not a valid task type")
                 .WithName("Task type");
 
-            RuleFor(model => model.PriorityId).NotNull().WithMessage("Priority type type cannot be null")
+            RuleFor(model => model.PriorityId).NotNull().WithMessage("This field is required.")
                 .InclusiveBetween(1, 3).WithMessage("This is not a valid priority type")
                 .WithName("Priority");
         }
